cbe28c9598e3da167a5ca2978dbf67fb8929f101
Matheus Tabarelli <12tabarelli@gmail.com>
2021-08-09 23:18:01 -0300

    Adicionando Classes de Teste com JUnit

--- src/main/java/org/generation/blogPessoal/model/Tema.java
+++ src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
@@ -20,7 +22,8 @@ public class Tema {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
-	@NotNull
+	@NotNull(message = "O atributo descrição é obrigatório")
+	@Size(min = 5, max = 250, message = "O atributo descrição deve conter no mínimo 05 e no máximo 250 caracteres")
 	private String descricao;
 	
 	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
